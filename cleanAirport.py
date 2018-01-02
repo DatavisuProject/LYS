938eafa333989d2a921e63ef63ecad902efb0f06
Brice <bricesimonletcher@yahoo.fr>
2018-01-02 23:58:21 +0100

    Travail sur la carte/itinéraire

--- cleanAirport.py
+++ cleanAirport.py
@@ -3,7 +3,7 @@ import os
 #Fichier avec les aérports, codes, coordonnées GPS
 airports = ""
 
-with open("/data/code_airports.txt", encoding = "utf-8") as file_airports:
+with open("data/airports.dat", encoding = "utf-8") as file_airports:
     for line in file_airports:
         line = line.split(',')
         if line[3] == '"United States"':
@@ -11,7 +11,7 @@ with open("/data/code_airports.txt", encoding = "utf-8") as file_airports:
             lon = round(float(line[7]),2)
             airports += line[1] + ',' + line[2] + ',' + line[4] + ',' + str(lat) + ',' + str(lon) + '\n'
 
-with open("/data/code_airports_modif.csv", "w", encoding = "utf-8") as file:
+with open("data/airports_reduced.dat", "w", encoding = "utf-8") as file:
     file.write('airport_name,city,code,lat,lon \n')
     file.write(airports)
 
